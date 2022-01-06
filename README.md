eff84296e97039d52f93a0dc3a3cca5f02e256e4
王奥斯 <9068149@qq.com>
2022-01-06 17:44:10 +0800

    增加模板

--- README.md
+++ README.md
@@ -1,31 +1,32 @@
 # AS-Cli
 
 #### 介绍
-AS-Cli是一个脚手架工具项目
+
+AS-Cli 是一个脚手架工具项目
+
+#### 使用说明
+
+1.  npm i as-yyds-cli -g (从 npm 全局安装 AS-Cli)
+1.  as -h (查看脚手架所有的指令)
+1.  as create name (创建项目)
 
 #### 本地调试
 
 1.  npm install (安装依赖)
-2.  npm link （进行全局link注册）
-2.  npm unlink （卸载全局link）
+2.  npm link （进行全局 link 注册）
+3.  npm unlink （卸载全局 link）
 
-#### 使用说明
-
-1.  npm i as-yyds-cli -g (从npm全局安装AS-Cli)
-1.  as -h (查看脚手架所有的指令)
-2.  as create name (创建项目)
-
-#### 使用的npm库
+#### 使用的 npm 库
 
 ```
 commander                      // 快速开发Nodejs命令行工具的package
 inquirer                       // 给输入命令行的用户提供一个好看的界面
 download-git-repo              // 从git下载项目
 ```
+
 #### 参与贡献
 
 1.  Fork 本仓库
 2.  新建 Feat_xxx 分支
 3.  提交代码
 4.  新建 Pull Request
-
--- bin/create.js
+++ bin/create.js
@@ -6,25 +6,38 @@ const inquirer = require('inquirer')
 const download = require('download-git-repo')
 
 module.exports = async function (name) {
-  inquirer
-    .prompt([
+  let res
+
+  const template = await inquirer.prompt([
+    {
+      name: 'list',
+      type: 'list',
+      message: '选择模板:',
+      choices: ['AS-Vue2Template', 'AS-Vue3Template','AS-Editor','AS-Editor-H5'],
+      default: 0
+    }
+  ])
+
+  if(template.list==='AS-Vue2Template'){
+     res = await inquirer.prompt([
       {
         name: 'list',
         type: 'list',
-        message: 'list',
+        message: '选择mobile or pc:',
         choices: ['mobile', 'pc'],
         default: 0
       }
     ])
-    .then(res => {
-      download(`github:qq9068149/AS-Vue2Template#${res.list}`, name, err => {
-        if (!err) {
-          // 可以输出一些项目成功的信息
-          console.info(`下载${res.list}分支 成功`)
-          return
-        }
-        // 可以输出一些项目失败的信息
-        console.info(`下载${res.list}分支 失败`)
-      })
-    })
+  }
+  
+  console.info(`选择完毕，创建项目中...`)
+  download(`github:qq9068149/${template.list}${res?'#'+res.list:''}`, name, err => {
+    if (!err) {
+      // 可以输出一些项目成功的信息
+      console.info(`创建${template.list}模板 成功`)
+      return
+    }
+    // 可以输出一些项目失败的信息
+    console.info(`创建${template.list}模板 失败`)
+  })
 }
